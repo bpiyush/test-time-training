bcf9a92d975c218849bda91ed6667eaf6968286d
Piyush Bagad <piyushnbagad11@gmail.com>
2020-12-20 18:13:13 +0530

    Code to download v6 of CIFAR-10.1

--- datasets/download/cifar-10.1.sh
+++ datasets/download/cifar-10.1.sh
@@ -3,7 +3,11 @@ folder=/scratch/users/piyushb/test-time-training/datasets/CIFAR-10.1/raw
 # download the data
 path=$folder/cifar10.1_v4_data.npy
 wget -O $path https://github.com/modestyachts/CIFAR-10.1/raw/master/datasets/cifar10.1_v4_data.npy
+path=$folder/cifar10.1_v6_data.npy
+wget -O $path https://github.com/modestyachts/CIFAR-10.1/raw/master/datasets/cifar10.1_v6_data.npy
 
 # download the labels
 path=$folder/cifar10.1_v4_labels.npy
 wget -O $path https://github.com/modestyachts/CIFAR-10.1/raw/master/datasets/cifar10.1_v4_labels.npy
+path=$folder/cifar10.1_v6_labels.npy
+wget -O $path https://github.com/modestyachts/CIFAR-10.1/raw/master/datasets/cifar10.1_v6_labels.npy
