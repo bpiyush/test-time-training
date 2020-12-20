dc12fe4bf733adfc8c50bdb2e0716b5acc485018
Piyush Bagad <piyush@wadhwaniai.org>
2020-12-20 16:31:22 +0530

    Adds constants file

---
+++ ttt/constants.py
@@ -0,0 +1,8 @@
+from os.path import join
+
+
+DATASET_DIR = "/scratch/users/piyushb/test-time-training/datasets/"
+DATASETS = {
+	"CIFAR-10-C": join(DATASET_DIR, "CIFAR-10-C"),
+	"CIFAR-10.1": join(DATASET_DIR, "CIFAR-10.1")
+}
