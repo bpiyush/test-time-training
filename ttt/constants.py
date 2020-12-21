a502d9e8953a9fb8b04ecd5ec79ebc540ceb636b
Piyush Bagad <piyushnbagad11@gmail.com>
2020-12-21 23:08:18 +0530

    Adds constant

--- ttt/constants.py
+++ ttt/constants.py
@@ -4,5 +4,6 @@ from os.path import join
 DATASET_DIR = "/scratch/users/piyushb/test-time-training/datasets/"
 DATASETS = {
 	"CIFAR-10-C": join(DATASET_DIR, "CIFAR-10-C"),
-	"CIFAR-10.1": join(DATASET_DIR, "CIFAR-10.1")
+	"CIFAR-10.1": join(DATASET_DIR, "CIFAR-10.1"),
+	"CIFAR-10": join(DATASET_DIR, "CIFAR-10")
 }
