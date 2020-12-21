8da4a581e482043f88e06e89f2d462f8ca9d9af5
Piyush Bagad <piyushnbagad11@gmail.com>
2020-12-21 20:06:47 +0530

    Adds target transformer for classification task

---
+++ ttt/data/target_transforms.py
@@ -0,0 +1,50 @@
+"""
+Defines all transforms to be applied on a label
+"""
+from typing import List, Any, Union, Tuple, Callable, Optional
+from ttt.factory import Factory
+
+
+class ClassificationTargetTransformer:
+    """
+    Transforms the input label to the appropriate target value
+    for single-label classification.
+    :param classes: list of relevant classes for classification
+    :type classes: List[Union[str, int, float]]
+    """
+    def __init__(self, classes: List[Union[str, int, float]]):
+        assert isinstance(classes, list)
+        self.classes = classes
+
+    def __call__(self, target:  List[Union[str, int, float]]) -> int:
+        # find the intersection between target and self.classes
+        intersection = [
+            _target for _target in target if _target in self.classes]
+
+        # ensure that only one of the relevant classes is present
+        # in the target at max
+        if len(intersection) > 1:
+            raise ValueError(
+                'target contains more than 1 overlapping class with self.classes')
+
+        # if one intersection, then return the corresponding index
+        if len(intersection) == 1:
+            return self.classes.index(intersection[0])
+
+        raise ValueError(
+            'target contains has no overlapping class with self.classes')
+
+
+annotation_factory = Factory()
+annotation_factory.register_builder(
+    "classification", ClassificationTargetTransformer)
+
+
+if __name__ == '__main__':
+    classes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    target_transformer = ClassificationTargetTransformer(classes=classes)
+
+    sample_label = [1.0]
+    label = target_transformer(sample_label)
+
+    assert label == 1
---
+++ ttt/factory.py
@@ -0,0 +1,39 @@
+"""Defines Factory"""
+from typing import Any
+
+
+class Factory:
+    """Factory class to build new objects"""
+
+    def __init__(self):
+        self._builders = dict()
+
+    def register_builder(self, key: str, builder: Any):
+        """Registers a new builder into the factory
+        Args:
+            key (str): key corresponding to the builder
+            builder (Any): Builder object
+        """
+        self._builders[key] = builder
+
+    def get_builder(self, key: str) -> Any:
+        """Returns the given builder corresponding to the key
+        Args:
+            key (str): key corresponding to the builder
+        """
+        return self._builders[key]
+
+    def create(self, key: str, **kwargs):
+        """Instantiates a new builder object, once it's registered
+        Args:
+            key (str): key corresponding to the builder
+            **kwargs: keyword arguments
+        Returns:
+            Any: Returns an object corresponding to the dataset builder
+        Raises:
+            ValueError: If dataset builder is not registered, raises an exception
+        """
+        builder = self._builders.get(key)
+        if not builder:
+            raise ValueError(key)
+        return builder(**kwargs)
